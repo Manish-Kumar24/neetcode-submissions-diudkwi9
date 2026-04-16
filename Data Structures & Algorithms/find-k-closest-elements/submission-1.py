28fa395cf8353d0765ab86dc039e16c0687be379
Manish-Kumar24 <148943231+Manish-Kumar24@users.noreply.github.com>
2026-04-16 18:13:08 +0530

    Add: find-k-closest-elements - submission-1

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        # Two Pointer -> O(n)/O(1)
+        # l, r = 0, len(arr) - 1
+        # while r - l + 1 > k:
+        #     if abs(arr[l] - x) > abs(arr[r] - x):
+        #         l += 1
+        #     else:
+        #         r -= 1
+        # return arr[l:r+1]
+
+        # Sliding Window + BS -> o(logn)/O(1)
+        l, r = 0, len(arr)-k
+        while l < r:
+            mid = (l + r) // 2
+            if x - arr[mid] > arr[mid+k] - x:
+                l = mid + 1
+            else:
+                r = mid
+        return arr[l:l+k]
