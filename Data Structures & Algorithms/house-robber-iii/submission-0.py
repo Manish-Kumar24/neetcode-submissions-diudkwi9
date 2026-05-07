6e6949818b831051f5a160f8c171cf33999f7fb9
Manish-Kumar24 <148943231+Manish-Kumar24@users.noreply.github.com>
2026-05-07 10:36:03 +0530

    Add: house-robber-iii - submission-0

---
+++ Data Structures & Algorithms/house-robber-iii/submission-0.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            if not node:
+                return (0, 0)
+            leftRob, leftSkip = dfs(node.left)
+            rightRob, rightSkip = dfs(node.right)
+            rob_this = node.val + leftSkip + rightSkip
+            skip_this = max(leftRob, leftSkip) + max(rightRob, rightSkip)
+            return (rob_this, skip_this)
+        return max(dfs(root))
